80330b25fb30c15435b4061d6cb91128575f731b
zetlock17 <95153376+zetlock17@users.noreply.github.com>
2025-03-03 21:52:03 +1000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow p-4">
+        <h1>Hello world</h1>
+      </main>
+    </div>
+  );
+};
+
+export default App;
