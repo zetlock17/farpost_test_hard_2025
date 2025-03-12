d46f01e3adc5aafa314bd299613a3fe9c8a21a16
zetlock17 <95153376+zetlock17@users.noreply.github.com>
2025-03-12 17:12:08 +1000

    add .env for fron

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+console.log('API_BASE_URL', API_BASE_URL);
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
